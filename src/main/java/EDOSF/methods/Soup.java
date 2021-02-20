b754c6e46619ab7fabc857edcffc7b622d289729
andreygnuchikh <44124200+andreygnuchikh@users.noreply.github.com>
2021-02-20 11:16:32 +0300

    added new url

--- src/main/java/EDOSF/methods/Soup.java
+++ src/main/java/EDOSF/methods/Soup.java
@@ -2,17 +2,18 @@ package EDOSF.methods;
 
 import okhttp3.*;
 
+
 public class Soup {
     public static String responseString;
 
-    public static String send(String srt){
+    public static String send(String url,String srt){
         try {
             OkHttpClient client = new OkHttpClient().newBuilder()
                     .build();
             MediaType mediaType = MediaType.parse("text/xml");
             RequestBody body = RequestBody.create(mediaType, srt);
             Request request = new Request.Builder()
-                    .url("http://10.48.0.13:8077/ws/iit/portal/DataExchangeService")
+                    .url(url)
                     .method("POST", body)
                     .addHeader("Content-Type", "text/xml")
                     .build();
